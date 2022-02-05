d411cfdeaa144ee7ab92a51cffa149684240fbee
Matthew Day <matt@matthewday.net>
2022-02-05 12:50:06 -0500

    add decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,15 @@ import { useState } from 'react';
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
+  const handleClick = (number) => {
+    setCount(count + number);
   };
 
   return (
     <>
       <p>The count is currently {count}</p>
-      <button onClick={handleClick}>+</button>
+      <button onClick={() => handleClick(1)}>+</button>
+      <button onClick={() => handleClick(-1)}>-</button>
     </>
   );
 };
