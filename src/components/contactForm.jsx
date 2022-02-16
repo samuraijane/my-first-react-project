d526e7477fba5438d753245fa846e7ba4a1df363
Matthew Day <matt@matthewday.net>
2022-02-15 21:27:41 -0500

    style form

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -20,35 +20,49 @@ const ContactForm = ({ action }) => {
 
   return (
     <form onSubmit={handleSubmit} >
-      <label>
-        <span>Full name</span>
-        <input onChange={handleChange} data-keyname="name" value={contact.name || ''} />
-      </label>
-      <label>
-        <span>Address</span>
-        <input onChange={handleChange} data-keyname="address" value={contact.address || ''} />
-      </label>
-      <label>
-        <span>City</span>
-        <input onChange={handleChange} data-keyname="city" value={contact.city || ''} />
-      </label>
-      <label>
-        <span>State</span>
-        <input onChange={handleChange} data-keyname="state" value={contact.state || ''} />
-      </label>
-      <label>
-        <span>Zip Code</span>
-        <input onChange={handleChange} data-keyname="zipcode" value={contact.zipcode || ''} />
-      </label>
-      <label>
-        <span>Telephone</span>
-        <input onChange={handleChange} data-keyname="phone" value={contact.phone || ''} />
-      </label>
-      <label>
-        <span>Email</span>
-        <input onChange={handleChange} data-keyname="email" value={contact.email || ''} />
-      </label>
-      <button type="submit">Submit</button>
+      <div className="contact-field">
+        <label>
+          <span>Full name</span>
+          <input onChange={handleChange} data-keyname="name" value={contact.name || ''} />
+        </label>
+      </div>
+      <div className="contact-field">
+        <label>
+          <span>Address</span>
+          <input onChange={handleChange} data-keyname="address" value={contact.address || ''} />
+        </label>
+      </div>
+      <div className="contact-field">
+        <label>
+          <span>City</span>
+          <input onChange={handleChange} data-keyname="city" value={contact.city || ''} />
+        </label>
+      </div>
+      <div className="contact-field">
+        <label>
+          <span>State</span>
+          <input onChange={handleChange} data-keyname="state" value={contact.state || ''} />
+        </label>
+      </div>
+      <div className="contact-field">
+        <label>
+          <span>Zip Code</span>
+          <input onChange={handleChange} data-keyname="zipcode" value={contact.zipcode || ''} />
+        </label>
+      </div>
+      <div className="contact-field">
+        <label>
+          <span>Telephone</span>
+          <input onChange={handleChange} data-keyname="phone" value={contact.phone || ''} />
+        </label>
+      </div>
+      <div className="contact-field">
+        <label>
+          <span>Email</span>
+          <input onChange={handleChange} data-keyname="email" value={contact.email || ''} />
+        </label>
+      </div>
+      <button className="btn" type="submit">Submit</button>
     </form>
   );
 };
