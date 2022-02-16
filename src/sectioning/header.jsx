9c2427bd5c0031345431d1cc0d3d866ca92a524b
Matthew Day <matt@matthewday.net>
2022-02-15 21:15:21 -0500

    style homepage and add routing for root

--- src/sectioning/header.jsx
+++ src/sectioning/header.jsx
@@ -3,8 +3,9 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <nav className="y-wrap">
-        <ul>
+      <nav className="y-wrap navs navs--justify">
+        <div><NavLink to="/">Home</NavLink></div>
+        <ul className="navs navs--items">
           <li><NavLink to="/add">Add Contact</NavLink></li>
           <li><NavLink to="/list">Contact List</NavLink></li>
         </ul>
